ac79729c7c021d648e3f215e37f53b72ed5bd9a2
RISVN <rizwanshaz2004@gmail.com>
2024-12-19 01:37:11 +0530

    'Linkedlist.pop()'

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -25,7 +25,35 @@ class LinkedList():
             self.tail.next=new_node
             self.tail=new_node
         self.length+=1
+    #removing end of element from the list
+    def pop(self):
+        if self.length==0:
+            return None
+        temp=self.head
+        pre=self.head  
+        while(temp.next):  #traversing the list and keeping track of prev for assigning tail before pop operation
+            pre=temp
+            temp=temp.next
+        self.tail=pre
+        self.tail.next=None
+        self.length -=1
+        if self.length==0:  #this case is after poping single element the head and tail still referenced to the element
+            self.head=None
+            self.tail=None
+        return temp.value  #returning poped value
 
+
+
+
+
+
+
+#test cases
 my_list=LinkedList(5)
 my_list.append(4)
+my_list.append(3)
 my_list.print_list()
+print('______')
+print(my_list.pop())
+print(my_list.pop())
+
